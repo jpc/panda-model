410564836321459c42f786f1f9cbf439e1386e44
Jakub Piotr Cłapa <jpc@loee.pl>
2022-05-17 17:27:57 +0200

    Switch to the exported model

--- app.py
+++ app.py
@@ -2,23 +2,9 @@ import gradio as gr
 from fastai.vision.all import *
 import skimage
 
-learn = load_learner('panda-model-1')
+learn = load_learner('models/panda-model-1.pth')
 
 labels = learn.dls.vocab
-
-def get_crops(img):
-    tile_size = 250
-    img = np.array(img)
-    crop = np.array(img.shape) // tile_size * tile_size; crop
-    imgc = img[:crop[0],:crop[1]]
-    imgc = imgc.reshape(imgc.shape[0] // tile_size, tile_size, imgc.shape[1] // tile_size, tile_size, 3)
-    xs, ys = (imgc.mean(axis=1).mean(axis=2).mean(axis=-1) < 252).nonzero()
-    if len(xs) == 0:
-        xs, ys = (imgc.mean(axis=1).mean(axis=2).mean(axis=-1)).nonzero()
-#     if len(xs) < 2: print("no data in image:", x)
-    pidxs = random.choices(list(range(len(xs))), k=36)
-    return PILImage.create(imgc[xs[pidxs],:,ys[pidxs],:].reshape(6,6,tile_size,tile_size,3).transpose(0,2,1,3,4).reshape(6*tile_size,6*tile_size,3))
-#     return imgc.mean(axis=1).mean(axis=2).mean(axis=-1)
 
 def predict(img):
     img = get_crops(PILImage.create(img))
